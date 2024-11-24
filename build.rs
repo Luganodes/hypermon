9ea86b1b20a0ec3ca58828fc5aaaef95a933e3ff
Suryansh <suryansh5060@gmail.com>
2024-11-24 15:07:45 +0900

    Remove parts of build.rs which won't work with github workflow

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
-use std::{
-    env, fs,
-    io::Read,
-    path::PathBuf,
-    process::{Command, Stdio},
-};
+use std::{env, fs, path::PathBuf};
 
 fn main() -> std::io::Result<()> {
     let config = std::fs::read_to_string("Cargo.toml")?;
@@ -22,39 +17,9 @@ fn main() -> std::io::Result<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = PathBuf::from(out_dir).join("version_file");
 
-    // Input buffers
-    let mut branch_buf = String::new();
-    let mut head_buf = String::new();
-
-    let _ = Command::new("git")
-        .arg("branch")
-        .arg("--show-current")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Couldn't get the current branch")
-        .stdout
-        .unwrap()
-        .read_to_string(&mut branch_buf);
-    let _ = Command::new("git")
-        .arg("rev-parse")
-        .arg("HEAD")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Couldn't get the current Git HEAD")
-        .stdout
-        .unwrap()
-        .read_to_string(&mut head_buf);
-
-    branch_buf.remove(branch_buf.len() - 1);
-    head_buf.remove(head_buf.len() - 1);
+    fs::write(&dest_path, format!("{}", version_str)).unwrap();
 
     println!("cargo:rerun-if-changed=Cargo.toml");
-
-    fs::write(
-        &dest_path,
-        format!("{} on {} with {}", version_str, branch_buf, head_buf),
-    )
-    .unwrap();
 
     Ok(())
 }
